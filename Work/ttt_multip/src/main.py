1eb63dbe94cb637f92f1c931ec7aebca12bbb147
Alyssa-star9 <miracleartist01@gmail.com>
2025-04-23 10:46:52 -0400

    SAVE

---
+++ Work/ttt_multip/src/main.py
@@ -0,0 +1,19 @@
+import socket
+import threading
+
+class TicTacToe:
+    def __init__(self):
+        self.board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+        self.turn = "X"
+        self.you = "X" # Person who runs the server
+        self.opponent = "O"
+        self.winner = None
+        self.end_game = False
+
+        self.counter = 0
+    
+    def host_game(self, host, port):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((host, port))
+        server.listen(1)
+
