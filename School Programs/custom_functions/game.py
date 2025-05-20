828b26c4022a1bbe3537667be38e4dc5f651d6c9
Alyssa-star9 <miracleartist01@gmail.com>
2025-05-20 09:18:14 -0400

    save

---
+++ School Programs/custom_functions/game.py
@@ -0,0 +1,6 @@
+# File Name: AD-1051350-game.py
+# Description: "Game Dice Generator"
+#              This program generates a random number between 1 and 6, simulating a dice roll.
+#              It then asks the user if they want to roll again. The program continues until the user chooses to stop.
+#              The results are printed.
+
